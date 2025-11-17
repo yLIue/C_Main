0096453789d2ce5783b5e751a92368d1664f9b6e
yLIue <2138347243@qq.com>
2025-11-17 11:53:24 +0800

    课堂写的链表,写完了,有创建,插入,删除,输入函数

--- Int/LinearList/LinkedList/List.h
+++ Int/LinearList/LinkedList/List.h
@@ -10,38 +10,73 @@
 
 typedef int ElemType;
 
-typedef struct Node{
+typedef struct Node {
     ElemType data;
     struct Node *next;
-}List,*Node;
+} List, *Node;
 
 static void beforeAppend(List *_l, ElemType _e) {
-    Node _newNode = (Node)malloc(sizeof(Node));
+    Node _newNode = (Node) malloc(sizeof(Node));
     _newNode->data = _e;
     _newNode->next = _l->next;
     _l->next = _newNode;
 }
 
-static Node Create() {
-    Node _node = (Node)malloc(sizeof(Node));
+static void append(List *_l, ElemType _e) {
+    Node _newNode = (Node) malloc(sizeof(Node));
+    _newNode->data = _e;
+    _newNode->next = NULL;
+
+    if (_l->next == NULL) {
+        _l->next = _newNode;
+        return;
+    }
+
+    Node _p = _l->next;
+    while (_p->next) _p = _p->next;
+    _p->next = _newNode;
+}
+
+static void insert(List *_l, int _position, ElemType _e) {
+    Node _p = _l->next;
+    for (int i = 1; i < _position-1; i++)
+        _p = _p->next;
+    Node _newNode = (Node) malloc(sizeof(Node));
+    _newNode->data = _e;
+    _newNode->next = _p->next;
+    _p->next = _newNode;
+}
+
+static void Remove(List *_l, int _position) {
+    Node _p = _l->next;
+    for (int i = 1; i < _position-1; i++)
+        _p = _p->next;
+    Node _remove = _p->next;
+    _p->next = _remove->next;
+    free(_remove);
+}
+
+
+static Node create() {
+    Node _node = (Node) malloc(sizeof(Node));
     ElemType _e;
     _node->next = NULL;
-    scanf("%d",&_e);
-    while (_e!=-1) {
-        beforeAppend(_node,_e);
-        scanf("%d",&_e);
+    scanf("%d", &_e);
+    while (_e != -1) {
+        // beforeAppend(_node,_e);
+        append(_node, _e);
+        scanf("%d", &_e);
     }
     return _node;
 }
 
-
-
 static void print(List *_l) {
     Node _node = _l->next;
-    while (_node!=NULL) {
-        printf("%d ",_node->data);
+    while (_node) {
+        printf("%d ", _node->data);
         _node = _node->next;
     }
+    printf("\n");
 }
 
 #endif //MAIN_LIST_H
--- Task/Task/Task_Temp.h
+++ Task/Task/Task_Temp.h
@@ -9,7 +9,23 @@
 #include "../../Int/LinearList/LinkedList/List.h"
 
 static void run() {
-    List *list = Create();
+    printf("输入链表结点的值,以-1结束:");
+    List *list = create();
+
+    printf("输出链表: ");
+    print(list);
+
+    printf("请输入插入位置和插入元素的值:");
+    int position,value;
+    scanf("%d %d",&position,&value);
+    printf("在第%d个节点前插入%d: ",position,value);
+    insert(list, position,value);
+    print(list);
+
+    printf("请输入删除位置:");
+    scanf("%d",&position);
+    printf("删除第%d个结点",position);
+    Remove(list,position);
     print(list);
 }
 
